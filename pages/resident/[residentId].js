a42acf38afc921de113b00543dea255d2c6b0da2
Ruben Paredes <ruben.pardes25@gmail.com>
2021-10-26 22:55:48 -0400

    Fix page without React Component exported

--- pages/resident/[residentId].js
+++ pages/resident/[residentId].js
@@ -1,15 +1,9 @@
 /**
  * Use https://nextjs.org/docs/migrating/from-react-router
  */
-/*
-import {useLocation} from 'react-router'
-
 function DetailPage() {
 
-    const thisUrl = useLocation()
-
-    return <h1>Unique ID {thisUrl.pathname}</h1>
+    return <h1>DetailPage</h1>
 }
 
 export default DetailPage
-*/
