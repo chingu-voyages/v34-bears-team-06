400e646c80a813d1ec1b05655ee178b9a13baa87
Ruben Paredes <ruben.pardes25@gmail.com>
2021-10-28 23:20:49 -0400

    Improve MenuWeek

--- components/MenuWeek.js
+++ components/MenuWeek.js
@@ -1,30 +1,103 @@
-import { useState, useEffect } from "react";
-import { Box, Image, Flex, Spacer, Badge } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  VStack,
+  HStack,
+  Text,
+} from "@chakra-ui/react";
 import menuData from "../utils/api/mealjson";
 
 export default function MenuWeek() {
+  const today = menuData.days[0];
+  const tomorrow = menuData.days[1];
+
   return (
-    <Box maxW="100%" border="1px" my="5px" borderRadius="md" overflow="hidden">
+    <Box
+      maxW="100%"
+      my="5px"
+      p="10px"
+      borderWidth="1px"
+      textAlign="center"
+      boxShadow="xl"
+      borderRadius="md"
+      overflow="hidden"
+    >
       <b>View This Week's Menu</b>
       <br />
       <b>{`${menuData.menu_title} (${menuData.easy_id})`}</b>
 
-      {/* Was working, but is not working now */}
-      {menuData.days.map((day) => {
-        let meal = (
-          <p
-            key={`${menuData.easy_id}-${day.day_number}`}
-            id={`${menuData.easy_id}-${day.day_number}`}
-          >
-            Day {day.day_number}:<br />
-            <b>{day.meals[0].meal_role}</b> - {day.meals[0].meal_title}
-            <br />
-            <b>{day.meals[1].meal_role}</b> - {day.meals[1].meal_title} <br />
-            <b>{day.meals[2].meal_role}</b> - {day.meals[2].meal_title}{" "}
-          </p>
-        );
-        return <Box padding="10px"> {meal} </Box>;
-      })}
+      <VStack>
+        <HStack h="120px">
+          <MealsSummary bg="yellow.300" meals={today.meals} />
+          {today.meals.map((meal, i) => {
+            const color = getCharkaUIColor("orange", i);
+
+            return <Meal key={meal.role} bg={color} {...meal} />;
+          })}
+        </HStack>
+        <HStack h="120px">
+          <MealsSummary bg="yellow.400" meals={today.meals} />
+          {tomorrow.meals.map((meal, i) => {
+            const color = getCharkaUIColor("orange", i, 4);
+            
+            return <Meal key={meal.role} bg={color} {...meal} />;
+          })}
+        </HStack>
+      </VStack>
     </Box>
+  );
+}
+
+function getCharkaUIColor(color, i, base = 3) {
+  let colorIndex = base - i;
+  // If colorIndex is smaller than 1, it has to re-assigned to other number between 9 and 1
+  // if (colorIndex < 1) {
+  // }
+  return "" + color + "." + colorIndex + "00";
+}
+
+function Meal({ meal_role, meal_title, ...props }) {
+  return (
+    <MealBox {...props}>
+      <Text fontWeight="bold">{meal_role}</Text>
+      <Text noOfLines={2}>{meal_title}</Text>
+    </MealBox>
+  );
+}
+
+function MealsSummary({ meals, ...props }) {
+  let protein_total = 0;
+  let carbs_total = 0;
+  let fats_total = 0;
+
+  meals.forEach((meal) => {
+    protein_total += meal.protein_offered;
+    carbs_total += meal.carbs_offered;
+    fats_total += meal.fat_offered;
+  });
+
+  return (
+    <MealBox {...props}>
+      <Text fontWeight="bold">Today</Text>
+      <Text>Protein: {protein_total}</Text>
+      <Text>Carbs: {carbs_total}</Text>
+      <Text>Fats: {fats_total}</Text>
+    </MealBox>
+  );
+}
+
+function MealBox({ children, boxProps, ...props }) {
+  return (
+    <Center
+      w="180px"
+      h="100%"
+      p="10px"
+      textAlign="center"
+      boxShadow="sm"
+      borderRadius="md"
+      {...props}
+    >
+      <Box {...boxProps}>{children}</Box>
+    </Center>
   );
 }
