716d2da0bbbd4a7de79b99746d59e212f6fd16b4
Ruben Paredes <ruben.pardes25@gmail.com>
2021-11-05 21:16:10 -0400

    Edit resident now correctly updates the resident - Add  API folder - Add loading indicator in dashboard - Implement update resident endpoint

--- pages/resident/dashboard.js
+++ pages/resident/dashboard.js
@@ -1,65 +1,47 @@
 import { useState, useEffect } from "react";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Center, Spinner } from "@chakra-ui/react";
 import TopDashboard from "components/TopDashboard";
 import MealTimeSidebar from "components/MealTimeSidebar";
 import CalorieGoals from "components/CalorieGoals";
 import MenuWeek from "components/MenuWeek";
 import CaloriesGraph from "components/CaloriesGraph"
+import { getResidents, getMenus } from "utils/api"
 
 // mock data
 import eatingHistory from "utils/api/eatingHistory";
 import mockMenu from "utils/api/mealjson";
 
 export default function Dashboard() {
-  const [userDB, setUserDB] = useState({
-    id: "1234",
-    first_name: "John",
-    last_name: "Doe",
-    date_of_birth: "1977-01-01T23:28:56.782+00:00",
-    weight: 75,
-    height: 175,
-  });
-  const [menu, setMenu] = useState({
-  menu_title: "Sample",
-  init_date: "November 28 1999 12:00:00",
-  days: []
-  })
-  async function getResident() {
-    const response = await fetch("/api/resident?first_name=John&populate=true");
-    console.log("[dashboard][getResident] response:", response);
-    const data = await response.json();
-    const firstResident = data.resident[0];
-    console.log("[dashboard][getResident] firstResident:", firstResident);
-    setUserDB(firstResident);
-  }
+  const [resident, setResident] = useState();
+  const [menu, setMenu] = useState()
+  
+  async function getData() {
+    const _resident = await getResidents({first_name: "John", populate: true})
+    const _menu = await getMenus({populate: true})
 
-  async function getMenu() {
-    const response = await fetch("/api/menu?populate=true");
-    console.log("[dashboard][getMenu] response:", response);
-    const data = await response.json();
-    const firstMenu = data.menus[0];
-    console.log("[dashboard][getMenu] firstMenu:", firstMenu);
-    setMenu(firstMenu)
-  }
+    console.log("[dashboard] _resident: ", _resident)
+    console.log("[dashboard] _menu: ", _menu)
 
+    setResident(_resident[0])
+    setMenu(_menu[0])
+  }
   useEffect(() => {
+    getData()
     console.log("first render")
-    getResident()
-    getMenu()
   }, [])
-
-  if (!menu || !menu.days || !menu.days[0]) {
-    return <p>Loading</p>
+  
+  if (!menu || !resident) {
+    return <Center h="100vh"><Spinner size="xl"/></Center>
   }
-
+  
   return (
     <div suppressHydrationWarning>
-      <TopDashboard userDB={userDB}/>
+      <TopDashboard resident={resident}/>
       <Flex justify="center" >
         <MealTimeSidebar menuData={menu || mockMenu} />
         <Flex direction="column">
-          <CaloriesGraph eatingHistory={userDB.eating_history || eatingHistory}/>
-          <CalorieGoals userDB={userDB} />
+          <CaloriesGraph eatingHistory={resident.eating_history || eatingHistory}/>
+          <CalorieGoals userDB={resident} />
           <MenuWeek menuData={menu || mockMenu}/>
         </Flex>
       </Flex>
