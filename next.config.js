6d53f2380aced24ec135aa8b2a3d7d50a4859c60
Ruben Paredes <ruben.pardes25@gmail.com>
2021-11-12 10:39:13 -0400

    Setup homepage to be "/resident"

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/resident',
+        permanent: true,
+      },
+    ]
+  },
 }
